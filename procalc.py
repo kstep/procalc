c7a5af0887be666da2202e605cad16d7c0123cd8
Konstantin Stepanov <me@kstep.me>
2010-12-27 16:48:01 +0200

    exit nicely on keyboard interrupt

--- procalc.py
+++ procalc.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 # coding: utf-8
 
+import sys
 from procalc.main import ProCalcApp
 
 if __name__ == '__main__':
     app = ProCalcApp()
-    app.run()
+    try:
+        app.run()
+    except KeyboardInterrupt:
+        sys.exit(2)
 
