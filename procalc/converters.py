3ffd71d818f4ec92de507c3c8ecb71af0e3054d7
Konstantin Stepanov <kstep@p-nut.info>
2010-12-28 20:55:42 +0200

    octal prefix is 0o to avoid ambiguity

--- procalc/converters.py
+++ procalc/converters.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import __builtin__ as __builtins__
+
 def bin(x):
     r = ''
     a = abs(x)
@@ -11,21 +13,34 @@ def bin(x):
         r = '-' + r
     return r
 
+def oct(x):
+    r = '0o'+__builtins__.oct(x).lstrip('-0')
+    if x < 0:
+        r = '-' + r
+    return r
+
+def hex(x):
+    r = __builtins__.hex(x).upper().replace('0X', '0x')
+    return r
+
 def dec(s):
     x = s.lstrip('-')
     if not x:
         return 0
 
-    if x.startswith('0'):
-        if x.startswith('0x'):
-            base = 16
-        elif x.startswith('0b'):
-            base = 2
-        else:
-            base = 8
-        r = int(x.lstrip('0bx') or '0', base)
+    if x.startswith('0x'):
+        base = 16
+    elif x.startswith('0b'):
+        base = 2
+    elif x.startswith('0o'):
+        base = 8
     else:
+        base = 10
+
+    if base == 10:
         r = (float if '.' in x else int)(x)
+    else:
+        r = int(x.lstrip('0bxo') or '0', base)
 
     return -r if s.startswith('-') else r
 
