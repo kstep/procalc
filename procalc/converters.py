5c42c31396bdb3f167aec104bb7d3bcbab93abae
Konstantin Stepanov <kstep@p-nut.info>
2010-12-29 00:54:59 +0200

    converters doctests

--- procalc/converters.py
+++ procalc/converters.py
@@ -3,6 +3,10 @@
 import __builtin__ as __builtins__
 
 def bin(x):
+    '''
+    >>> bin(123)
+    '0b1111011'
+    '''
     r = ''
     a = abs(x)
     while a:
@@ -14,16 +18,38 @@ def bin(x):
     return r
 
 def oct(x):
+    '''
+    >>> oct(123)
+    '0o173'
+    '''
     r = '0o' + __builtins__.oct(x).lstrip('-0')
     if x < 0:
         r = '-' + r
     return r
 
 def hex(x):
+    '''
+    >>> hex(123)
+    '0x7B'
+    '''
     r = __builtins__.hex(x).upper().replace('0X', '0x')
     return r
 
 def dec(s):
+    '''
+    >>> dec("0xAA"), dec("0o100"), dec("0b101")
+    (170, 64, 5)
+    >>> dec("0b123")
+    Traceback (most recent call last):
+        ...
+    ValueError: invalid literal for int() with base 2: '123'
+    >>> dec("0o108")
+    Traceback (most recent call last):
+        ...
+    ValueError: invalid literal for int() with base 8: '108'
+    >>> dec(hex(123)), dec(oct(123)), dec(bin(123))
+    (123, 123, 123)
+    '''
     x = s.lstrip('-')
     if not x:
         return 0
