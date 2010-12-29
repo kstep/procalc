8e50d88c0149bd60a16a128d3c806fb723e73d1d
Konstantin Stepanov <kstep@p-nut.info>
2010-12-29 20:36:45 +0200

    converters support of floating point numbers in different bases

--- procalc/converters.py
+++ procalc/converters.py
@@ -1,80 +1,84 @@
 # coding: utf-8
 
-import __builtin__ as __builtins__
-import sys
+import struct
 
-def bfrac(x, p=5, b=2):
+def bits(n):
     '''
-    >>> bfrac(0.25)
-    '.01'
-    >>> bfrac(0.5)
-    '.1'
-    >>> bfrac(0.625)
-    '.101'
-    >>> bfrac(0.75)
-    '.11'
-    >>> bfrac(0.125)
-    '.001'
-    >>> bfrac(0.0625, b=16)
-    '.1'
-    '''
-    x -= int(x)
-    r = ''
-    while x > 0 and len(r) < p:
-        x *= b
-        i = int(x)
-        r += chr(55 + i) if i > 9 else str(i)
-        x -= i
-    return '.' + r if r else ''
+    Get number of bits in integer number.
 
-def with_floating_version(base):
-    def decorator(func):
-        def wrapper(x, p=5):
-            return func(int(x)) + bfrac(x, p, base)
-        wrapper.__doc__ = func.__doc__
-        wrapper.__name__ = func.__name__ + 'f'
-        setattr(sys.modules[__name__], wrapper.__name__, wrapper)
-        return func
-    return decorator
+    >>> bits(10)
+    4
+    '''
+    c = 0
+    while n:
+        c += 1
+        n >>= 1
+    return c
 
-@with_floating_version(2)
-def bin(x):
+def basef(x, base=2):
     '''
-    >>> bin(123)
-    '0b1111011'
-    '''
-    r = ''
-    a = abs(x)
-    while a:
-        r = str(a & 1) + r
-        a = a >> 1
-    r = '0b' + (r or '0')
-    if x < 0:
-        r = '-' + r
-    return r
+    Convert from decimal floating point number to string representation of some base.
+    base := 2 | 8 | 16
 
-@with_floating_version(8)
-def oct(x):
+    >>> basef(10)
+    '0b1010'
+    >>> basef(0.25)
+    '0b0.01'
+    >>> basef(10.5)
+    '0b1010.1'
+    >>> basef(10.0625, 16)
+    '0xA.1'
+    >>> basef(8.125, 8)
+    '0o10.1'
     '''
-    >>> oct(123)
-    '0o173'
-    '''
-    r = '0o' + __builtins__.oct(x).lstrip('-0')
-    if x < 0:
-        r = '-' + r
-    return r
+    prefix = ({2: '0b', 8: '0o', 16: '0x'}).get(base)
+    bitn = bits(base - 1)
 
-@with_floating_version(16)
-def hex(x):
-    '''
-    >>> hex(123)
-    '0x7B'
-    '''
-    r = __builtins__.hex(x).upper().replace('0X', '0x')
-    return r
+    def tobin(x):
+        s = ''
+        while x:
+            s = str(x & 1) + s
+            x >>= 1
+        return s
+
+    v = "".join(tobin(ord(i)).rjust(8, '0') for i in reversed(struct.pack('d', x)))
+    sign, exp, mant = int(v[0]), int(v[1:12], 2) - 1023 + 1, '1' + v[12:] 
+
+    sign, intg, frac = '-' if sign else '', '', ''
+    if exp < 0:
+        frac = ('0' * -exp) + mant
+        intg = '0'
+
+    elif exp > 0:
+        intg = mant[0:exp]
+        frac = mant[exp:]
+
+    if base > 2:
+        i, f = '', ''
+        while intg:
+            d, intg = int(intg[-bitn:], 2), intg[:-bitn]
+            d = str(d) if d < 10 else chr(d + 55)
+            i = d + i
+        while frac:
+            d, frac = int(frac[:bitn], 2), frac[bitn:]
+            d = str(d) if d < 10 else chr(d + 55)
+            f = f + d
+
+    else:
+        i, f = intg, frac
+
+    if f:
+        f = '.' + f
+
+    return sign + prefix + i + f.rstrip('.0')
+
+hex = lambda x: basef(x, 16)
+oct = lambda x: basef(x, 8)
+bin = lambda x: basef(x, 2)
 
 def dec(s):
     '''
+    Convert from string number representation of some base to decimal floating point.
     >>> dec("0xAA"), dec("0o100"), dec("0b101")
     (170, 64, 5)
     >>> dec("0b123")
@@ -95,6 +99,7 @@ def dec(s):
         return 0
 
     base = ({'0x': 16, '0o': 8, '0b': 2}).get(x[0:2], 10)
+    bitn = bits(base - 1)
 
     if base == 10:
         return (float if '.' in s else int)(s)
@@ -102,7 +107,7 @@ def dec(s):
         i, _, f = x.lstrip('0bxo').partition('.')
         r = int(i or '0', base)
         if f:
-            r += sum(float(int(n, base)) / base ** (m + 1) for m, n in enumerate(f))
+            r += sum(float(int(n, base)) / (base << bitn * m) for m, n in enumerate(f))
 
     return -r if s.startswith('-') else r
 
