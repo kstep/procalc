3483ca42d6649053f97dcd5275e440672578db1f
Konstantin Stepanov <me@kstep.me>
2010-12-29 02:53:13 +0200

    mass statistical operations

--- procalc/operations.py
+++ procalc/operations.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 
-__all__ = ['op_noop', 'op_oper', 'op_add', 'op_sub', 'op_mul', 'op_div', 'op_mod', 'op_pow', 'op_or', 'op_xor', 'op_and', 'op_andnot', 'op_not', 'op_shl', 'op_shr']
+__all__ = [
+            'op_noop', 'op_oper',  # service
+            'op_add', 'op_sub', 'op_mul', 'op_div','op_mod', 'op_pow',  # standard
+            'op_or', 'op_xor', 'op_and', 'op_andnot', 'op_not', 'op_shl', 'op_shr',  # bitwise
+            'op_sum', 'op_prod', 'op_mean', 'op_stdev'  # statistics
+            ]
 
 from procalc.converters import dec
 
@@ -9,14 +14,15 @@ class OperationError(ValueError):
 
 def operation(name, prio, *types):
     def decorator(func):
+        rtypes = tuple(reversed(types))
         def wrapper(stack):
             try:
-                args = [t(stack.pop_op()) for t in reversed(types)]
+                args = [t(stack.pop_op()) for t in rtypes]
                 args.reverse()
-                result = func(*args)
             except ValueError:
                 raise OperationError("Arguments type mismatch for %s(%s)" % (name, ", ".join(map(lambda t: t.__name__, types))))
 
+            result = func(*args)
             if result is None:
                 pass
             elif isinstance(result, tuple):
@@ -28,6 +34,47 @@ def operation(name, prio, *types):
         wrapper.op_prio = prio
         return wrapper
     return decorator
+
+def operation_for_all(name, prio, type_):
+    def decorator(func):
+        def wrapper(stack):
+            try:
+                args = [type_(item) for item in stack]
+                args.reverse()
+            except ValueError:
+                raise OperationError("Argument type mismatch for %s(%s, ...)" % (name, type_.__name__))
+
+            result = func(*args)
+            if result is None:
+                pass
+            elif isinstance(result, tuple):
+                for r in reversed(result):
+                    stack.push(r)
+            else:
+                stack.push(result)
+        wrapper.op_name = name
+        wrapper.op_prio = prio
+        return wrapper
+    return decorator
+
+@operation_for_all('Σ', -2, dec)
+def op_sum(*args):
+    return sum(args)
+
+@operation_for_all('Π', -1, dec)
+def op_prod(*args):
+    return reduce(lambda x, y: x * y, args, 1)
+
+@operation_for_all('μ', -3, dec)
+def op_mean(*args):
+    return sum(args) / len(args)
+
+@operation_for_all('σ', -3, dec)
+def op_stdev(*args):
+    import math
+    mean = sum(args) / len(args)
+    disp = sum((x - mean) ** 2 for x in args) / len(args)
+    return math.sqrt(disp)
 
 @operation('', 0)
 def op_noop():
